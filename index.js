eca2d145e4f2d582aae64d8fb50eae36325d3675
amandasuzzanne <amandasuzanne12@gmail.com>
2024-04-08 20:34:12 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,4 +1,103 @@
 /* Your Code Here */
+function createEmployeeRecord(arr) {
+    let arrtoObj = {};
+    arrtoObj.firstName = arr[0];
+    arrtoObj.familyName = arr[1];
+    arrtoObj.title = arr[2];
+    arrtoObj.timeInEvents = [];
+    arrtoObj.timeOutEvents = [];
+    if (typeof arr[3] === 'number') {
+        arrtoObj.payPerHour = arr[3]
+        return arrtoObj;
+    } else {
+        console.error("Last value in the array must be a numb.")
+    }
+}
+
+function createEmployeeRecords(arrays) {
+    let bigArrtoObj = []
+    for (let arr of arrays) {
+        bigArrtoObj.push(createEmployeeRecord(arr))
+    }
+    return bigArrtoObj;
+}
+
+
+function createTimeInEvent(record, dateStamp) {
+    let [date, time] = dateStamp.split(' ')
+    let hour = parseInt(time)
+
+    if (parseInt(hour) < 0 || parseInt(hour) > 2359) {
+         console.error("Hour must be in 24 Hour format.")
+    }
+
+    let timeInEvent = {
+        type: "TimeIn",
+        hour: hour,
+        date: date,
+    }
+
+    record.timeInEvents.push(timeInEvent)
+    return record;
+}
+
+function createTimeOutEvent(record, dateStamp) {
+    let [date, time] = dateStamp.split(' ')
+    let hour = parseInt(time)
+
+    if (parseInt(hour) < 0 || parseInt(hour) > 2359) {
+        console.error("Hour must be in 24 Hour format.")
+    }
+
+    let timeOutEvent = {
+        type: "TimeOut",
+        hour: parseInt(hour),
+        date: date,
+    }
+
+    record.timeOutEvents.push(timeOutEvent)
+    return record;
+}
+function hoursWorkedOnDate(record, date){
+    const recTimeInEvent = record.timeInEvents.find(event => event.date === date)
+    const recTimeOutEvent = record.timeOutEvents.find(event => event.date === date)
+    if(recTimeInEvent  && recTimeOutEvent) {
+          const hourTimeInEvent = recTimeInEvent.hour
+          const hourTimeOutEvent = recTimeOutEvent.hour
+          const hoursWorked = (hourTimeOutEvent - hourTimeInEvent)/100
+          return hoursWorked
+    } else{
+        console.error("Date must be the same as the date in the timeInEvent and timeOutEvent.");
+        return null;
+    }
+
+}
+
+function wagesEarnedOnDate(record, date){
+    const hoursWorked = hoursWorkedOnDate(record, date)
+    const payPerHour = record.payPerHour
+    const wagesEarned = hoursWorked * payPerHour
+    return wagesEarned
+}
+
+function allWagesFor(record){
+    let totalOfWages = 0;
+     record.timeInEvents.forEach(event => {
+        const date = event.date
+        const wagesPerDate = wagesEarnedOnDate(record, date);
+        totalOfWages += wagesPerDate
+    })
+    return totalOfWages
+
+}
+
+function calculatePayroll(allRecords){
+    let everyWages = 0;
+    for (let record of allRecords) {
+        everyWages += allWagesFor(record)
+    }
+    return everyWages
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
